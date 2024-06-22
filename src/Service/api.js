eb035f3ce2e3310851e3e8f0f863ce5df0661e4a
Sergio Lezama <98142685+SergioSLO@users.noreply.github.com>
2024-06-22 00:32:25 -0500

    Update api.js

---
+++ src/Service/api.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+
+const url = "http://google.com"
+
+
+
+const conver = (data) => {
+    const precio = Number(data.price)
+    const cantidad = Number(data.quantity)
+    return(
+        {...data,
+            price:precio,
+            quantity:cantidad,
+        }
+    )
+}
+
+const header = () => {
+    const token = localStorage.getItem('token');
+    return {headers: {Authorization : `Bearer ${token}`}};
+}
+
+const paginacion = (skip, limit) => {
+    let config = header();
+    config = {
+        ...config,
+        params:{
+            skip,
+            limit
+        }
+    };
+    return config;
+}
+
+export const fetchLogIn = async (body) => {
+    const response = await axios.post(`${url}/api/auth/login`,body)
+    console.log("LogInResponse")
+    console.log(response)
+    return response;
+}
+export const fetchRegister = async (body) => {
+    const response = await axios.post(`${url}/api/auth/register`,body)
+    console.log("RegisterResponse")
+    console.log(response)
+    return response;
+}
+export const fetchCreateProduct = async (body) => {
+    const config = header();
+    const response = await axios.post(`${url}/api/products`, body, config)
+    console.log("Crear Producto")
+    console.log(response)
+    return response;
+}
+export const fetchGetAllProducts = async (skip, limit) =>{
+    const config = paginacion(skip, limit)
+    const response = await axios.get(`${url}/api/products/`, config)
+    console.log("GetAllProducts")
+    console.log(response)
+    return response;
+}
+export const fetchGetProduct = async (product_id) =>{
+    const config = header();
+    const response = await axios.get(`${url}/api/products/${product_id}`, config)
+    console.log("GetProduct")
+    console.log(response)
+    return response;
+}
+export const fetchPutProduct = async (product_id, body)=>{
+    const config = header();
+    const response = await axios.put(`${url}/api/products/${product_id}`, body, config)
+    console.log("PutProduct")
+    console.log(response)
+    return response;
+}
+export const fetchDeleteProduct = async (product_id) =>{
+    const config = header();
+    const response = await axios.delete(`${url}/api/products/${product_id}`, config)
+    console.log("DeleteProduct")
+    console.log(response)
+    return response;
+}
