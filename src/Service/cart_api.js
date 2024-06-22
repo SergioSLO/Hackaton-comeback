08f11bbe73160e3e3380f3474749533b4e3ab7d0
BadiRodriguez <166084491+Badi-Rodriguez@users.noreply.github.com>
2024-06-22 16:18:00 -0500

    Badi_push 3
    Major improvment to the api module and cart_api module.
    
    + Made it so the api.js file can now share its header() function with the export denominator
    
    + cart_api now checks the data token of each command, requiring user role.

--- src/Service/cart_api.js
+++ src/Service/cart_api.js
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import {url} from "./api.js";
+import {header} from "./api.js";
 
 // Cart operations = = = = = = = = = =
 
@@ -9,7 +10,7 @@ import {url} from "./api.js";
 export const fetchCartBuy = async (userId) => { // This takes only the userID
     const data = {userId}; // Should be formated
     console.log("Executing buy operation . . .")
-    const response = await axios.post(`${url}/buy`, data);
+    const response = await axios.post(`${url}/buy`, data, header());
     console.log(`Operation line for [${data}]: ${response.status}`); // Point
     console.log(response); // Data
     return response;
@@ -19,7 +20,7 @@ export const fetchCartBuy = async (userId) => { // This takes only the userID
 export const fetchCartAddItem = async (itemID, userID) => { // This takes itemID and userID
     const data = {itemID, userID}; // Should be formated
     console.log("Adding item to cart . . .")
-    const response = await axios.post(`${url}/cart`, data);
+    const response = await axios.post(`${url}/cart`, data, header());
     console.log(`Item data: ${response}`); // Check data
     return response;
 }
@@ -28,17 +29,18 @@ export const fetchCartAddItem = async (itemID, userID) => { // This takes itemID
 export const fetchCartDeleteItem = async (itemID, userID) => { // This takes itemID and userID
     const data = {itemID, userID}; // Should be formated
     console.log("Deleting item from cart . . .")
-    const response = await axios.delete(`${url}/cart`);
+    const response = await axios.delete(`${url}/cart`, data, header());
     console.log(`Item data for [${data}]: ${response}`); // Check data
     return response;
 }
 
 // GET /cart/{userId}
 export const fetchCart = async (userId)=> {
+    const data = {userId};
     const route = `cart/${userId}`; // Route denotation
     console.log("Fetching cart contents . . .")
     console.log(`TARGET: [${userId}]`)
-    const response = await axios.get(`${url}/${route}`);
+    const response = await axios.get(`${url}/${route}`,data , header());
     console.log(`Item data: ${response}`); // Check data
     return response;
 }
