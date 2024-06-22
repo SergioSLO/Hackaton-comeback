943699fe75be4d8abf9d683521614640499041d7
joruughen <166084525+joruughen@users.noreply.github.com>
2024-06-22 16:20:09 -0500

    sisiii

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -84,6 +84,25 @@ export const Signup = () => {
                         </div>
 
                         <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="role" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Role(Admin o Cliente)
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    onChange={handleInputChange}
+                                    id="role"
+                                    name="role"
+                                    type="role"
+                                    autoComplete="current-role"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 bg-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
                             <button
                                 type="submit"
                                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
--- src/Service/apiLoginRegister.js
+++ src/Service/apiLoginRegister.js
@@ -1,11 +1,20 @@
 import axios from "axios";
+import { jwtDecode } from 'jwt-decode';
 //prueba
 const url = 'https://cepnq6rjbk.execute-api.us-east-1.amazonaws.com';
 
+export const getRoleBasedOnToken = () => {
+    const token = localStorage.getItem('token');
+    const decodedToken = jwtDecode(token);
+    return decodedToken.role;
+}
+
 export const fetchLogIn = async (body) => {
-    const response = await axios.post(`${url}/auth/login`,body)
+    const response = await axios.post(`${url}/auth/login`, body)
     console.log("LogInResponse")
     console.log(response)
+    localStorage.setItem("token", response.data.token)
+    localStorage.setItem("role", getRoleBasedOnToken(response.data.token))
     return response;
 }
 export const fetchRegister = async (body) => {
