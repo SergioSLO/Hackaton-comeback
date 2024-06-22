dd1484315f98bf08ddc64cc39f820e0803ab147b
BadiRodriguez <166084491+Badi-Rodriguez@users.noreply.github.com>
2024-06-22 16:36:44 -0500

    Badi_push 5
    I cleaned your shit bro >:v
    
    + Major improvement of all modules as to make the working area cleaner.

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,7 +1,11 @@
+// Let us begin
+
+// Imports, exports and constants
+
 import axios from "axios";
-//prueba
 export const url = 'https://cepnq6rjbk.execute-api.us-east-1.amazonaws.com';
 
+// Function for the fetching of a GET product operation
 export const fetchGetProducts = async (lastKey, limit) => {
     const response = await axios.get(`${url}/items?limit=${limit}&lastKey=${lastKey}`)
     console.log("FetchResult")
@@ -9,8 +13,8 @@ export const fetchGetProducts = async (lastKey, limit) => {
     return response;
 }
 
-//real
-const conver = (data) => {
+// Create Convertor
+const conver = (data) => { // Make conversion of the initial data contents
     const precio = Number(data.price)
     const cantidad = Number(data.quantity)
     return(
@@ -21,35 +25,42 @@ const conver = (data) => {
     )
 }
 
+// Declare header for token acquirement and role verification
 export const header = () => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token'); // Acquire token
     return {headers: {Authorization : `Bearer ${token}`}};
 }
 
+// Function declared for pagination.
 const paginacion = (skip, limit) => {
-    let config = header();
+    let config = header(); // Config declaration for the header file.
     config = {
-        ...config,
+        ...config, // Get the configuration parameter
         params:{
             skip,
             limit
         }
     };
-    return config;
+    return config; // Return the config
 }
 
+// Exportable function for fetching the Log-In operation
 export const fetchLogIn = async (body) => {
     const response = await axios.post(`${url}/api/auth/login`,body)
     console.log("LogInResponse")
     console.log(response)
     return response;
 }
+
+// Exportable function for fetching the Register operation.
 export const fetchRegister = async (body) => {
     const response = await axios.post(`${url}/api/auth/register`,body)
     console.log("RegisterResponse")
     console.log(response)
     return response;
 }
+
+// Exportable function for fetching the creation of products.
 export const fetchCreateProduct = async (body) => {
     const config = header();
     const response = await axios.post(`${url}/api/products`, body, config)
@@ -57,6 +68,8 @@ export const fetchCreateProduct = async (body) => {
     console.log(response)
     return response;
 }
+
+// Exportable function for the fetching the information of all products.
 export const fetchGetAllProducts = async (skip, limit) =>{
     const config = paginacion(skip, limit)
     const response = await axios.get(`${url}/api/products/`, config)
@@ -64,6 +77,8 @@ export const fetchGetAllProducts = async (skip, limit) =>{
     console.log(response)
     return response;
 }
+
+// Exportable function for the GET of a product.
 export const fetchGetProduct = async (product_id) =>{
     const config = header();
     const response = await axios.get(`${url}/api/products/${product_id}`, config)
@@ -71,6 +86,8 @@ export const fetchGetProduct = async (product_id) =>{
     console.log(response)
     return response;
 }
+
+// Exportable function for the placement (PUT) of a product.
 export const fetchPutProduct = async (product_id, body)=>{
     const config = header();
     const response = await axios.put(`${url}/api/products/${product_id}`, body, config)
@@ -78,6 +95,8 @@ export const fetchPutProduct = async (product_id, body)=>{
     console.log(response)
     return response;
 }
+
+// Exportable function for the deletion of products.
 export const fetchDeleteProduct = async (product_id) =>{
     const config = header();
     const response = await axios.delete(`${url}/api/products/${product_id}`, config)
